9ed4fe9e489145583b45033f070aa501e6c7dd0a
Trần Xuân Hòa <105307835+tranxuanhoa12@users.noreply.github.com>
2024-07-03 17:45:14 +0700

    Add files via upload

---
+++ PlayMusic/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMusic.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User/Manage
+        public ActionResult Manage()
+        {
+            return View();
+        }
+    }
+}
